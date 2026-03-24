c84907cd577448e3bbdebb264026ceeec806e6e3
Richard VCD <richardvincentius2112@gmail.com>
2026-03-24 15:50:24 +0700

    Membuat dao Inventaris
    Disini menggunakan Store Procedure (SP) sp_insert_inventaris yang sudah ada di SQL untuk menambah barang.

---
+++ app/dao/InventarisDao.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Dao;
+
+use App\Model\Inventaris;
+use PDO;
+
+class InventarisDao {
+    public function showInventarisByKamar($id_kamar) {
+        $link = \PDOUtil::createMySQLConnection();
+        $query = "SELECT * FROM inventaris_kamar WHERE id_kamar = ?";
+        $stmt = $link->prepare($query);
+        $stmt->bindValue(1, $id_kamar);
+        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Inventaris::class);
+        $stmt->execute();
+        $link = null;
+        return $stmt->fetchAll();
+    }
+
+    public function addInventaris(Inventaris $inventaris) {
+        $link = \PDOUtil::createMySQLConnection();
+        // Menggunakan SP wajib sesuai aturan tugas [cite: 34]
+        $query = "CALL sp_insert_inventaris(?, ?, ?)";
+        $stmt = $link->prepare($query);
+        $stmt->bindValue(1, $inventaris->getIdKamar());
+        $stmt->bindValue(2, $inventaris->getNamaBarang());
+        $stmt->bindValue(3, $inventaris->getKondisiBarang());
+        $result = $stmt->execute();
+        $link = null;
+        return $result;
+    }
+}
